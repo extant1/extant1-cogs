ea14871072124b1f95aa8d8747f6237a4fb40123
Scott <ext.whiskey@gmail.com>
2018-07-23 23:01:26 -0400

    setting

--- serverquery/serverquery.py
+++ serverquery/serverquery.py
@@ -90,18 +90,18 @@ class ServerQuery:
 
     @_server.command(name="ip", pass_context=True)
     async def _ip(self, ctx, ip: str = None):
-        """Add a game server ip to this discord."""
         self._set_setting(ctx, "ip", ip)
+        await self.bot.say("Setting server query ip to: " + ip)
 
     @_server.command(name="port", pass_context=True)
     async def _port(self, ctx, port: int = None):
-        """Add a game server port to this discord."""
         self._set_setting(ctx, "port", port)
+        await self.bot.say("Setting server query port to: " + port)
 
     @_server.command(name="gm", pass_context=True)
     async def _gm(self, ctx, role: str = None):
-        """Add the discord role that is an admin for this discord."""
         self._set_setting(ctx, "discord_gm_role", role)
+        await self.bot.say("Setting server query GM role to: " + role)
 
 
 def check_folders():
