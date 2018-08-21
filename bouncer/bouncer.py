3a06a250ed86b70a50a380bfe9f0b429414b6c77
Scott <ext.whiskey@gmail.com>
2018-08-20 23:05:45 -0400

    ...

--- bouncer/bouncer.py
+++ bouncer/bouncer.py
@@ -146,7 +146,7 @@ class Bouncer:
     async def on_message_edit(self, before, after):
         settings = self._get_settings(after.server)
         logger.info("message edited")
-        print(settings)
+        print(settings['ENABLED'])
         if settings is not None and settings['ENABLED'] is not False:
             logger.info("{} changed the message {} to {}.".format(after.author.display_name))
             channel = discord.utils.get(before.server.channels, name=str(settings['CHANNEL']),
