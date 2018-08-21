aba2fd807a172228ff337e863abd7e7d51288acf
Scott <ext.whiskey@gmail.com>
2018-08-21 01:33:23 -0400

    ...

--- bouncer/bouncer.py
+++ bouncer/bouncer.py
@@ -146,11 +146,11 @@ class Bouncer:
     async def on_message_edit(self, before, after):
         settings = self._get_settings(before.server)
         if settings is not None and settings['ENABLED']:
-            if after.call is MessageType.pins_add:
-                logger.info("pins add")
-                return
             if len(after.embeds) is not 0:
                 logger.info("embeds: {}".format(after.embeds))
+                return
+            if after.call is MessageType.pins_add:
+                logger.info("messaged was pinned:  {}".format(after.content))
                 return
             if after.call is MessageType.default:
                 logger.info("{} changed the message {} to {}.".format(after.author.display_name, before.content,
