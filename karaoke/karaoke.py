d1a1f6f2d7498533f633074fc9227fd4711da4ce
Whiskey Maple <ext.whiskey@gmail.com>
2019-04-24 23:05:25 -0400

    ----

--- karaoke/karaoke.py
+++ karaoke/karaoke.py
@@ -69,7 +69,7 @@ class Karaoke:
         pass
 
     # needs karaoke role permission
-    @_karaoke.command(pass_context=True, no_pm=True, aliases=["r"])
+    @_karaoke.command(pass_context=True, no_pm=True)
     async def reset(self, ctx):
         pass
 
