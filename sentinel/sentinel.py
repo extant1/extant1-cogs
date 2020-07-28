d8dbede1e6e1c955b2643060b5d9da045a4f8ad4
Whiskey <ext.whiskey@gmail.com>
2020-07-27 22:39:48 -0400

    -channel context

--- sentinel/sentinel.py
+++ sentinel/sentinel.py
@@ -35,8 +35,8 @@ class Sentinel(commands.Cog):
             # channel = discord.utils.get(member.guild.channels,
             #                             name=str(await self.config.guild(member.guild).channel()),
             #                             type=ChannelType.text)
-            channel = discord.utils.find(lambda c: c.name == (await self.config.guild(member.guild).channel()),
-                                         member.guild.channels)
+            channel_name = await self.config.guild(member.guild).channel()
+            channel = discord.utils.find(lambda c: c.name == channel_name, member.guild.channels)
             if (datetime.utcnow() - member.created_at) < timedelta(1):
                 message = "\nWARNING!!!  ACCOUNT IS LESS THAN ONE DAY OLD."
             elif (datetime.utcnow() - member.created_at) < timedelta(7):
@@ -59,8 +59,8 @@ class Sentinel(commands.Cog):
         toggle_leave = await self.config.guild(member.guild).toggle_leave()
         if enabled and toggle_leave:
             # logger.info("{} left the server.".format(member.display_name))
-            channel = discord.utils.find(lambda c: c.name == (await self.config.guild(member.guild).channel()),
-                                         member.guild.channels)
+            channel_name = await self.config.guild(member.guild).channel()
+            channel = discord.utils.find(lambda c: c.name == channel_name, member.guild.channels)
             # channel = discord.utils.get(member.guild.channels,
             #                             name=str(await self.config.guild(member.guild).channel()),
             #                             type=ChannelType.text)
@@ -78,8 +78,8 @@ class Sentinel(commands.Cog):
         toggle_ban = await self.config.guild(member.guild).toggle_ban()
         if enabled and toggle_ban:
             # logger.info("{} was banned from the server.".format(member.display_name))
-            channel = discord.utils.find(lambda c: c.name == (await self.config.guild(guild).channel()),
-                                         guild.channels)
+            channel_name = await self.config.guild(member.guild).channel()
+            channel = discord.utils.find(lambda c: c.name == channel_name, guild.channels)
             # channel = discord.utils.get(guild.channels, name=str(await self.config.guild(guild).channel()),
             #                             type=ChannelType.text)
             embed = discord.Embed(title="Member Banned", color=0xffff00)
@@ -96,8 +96,8 @@ class Sentinel(commands.Cog):
         toggle_unban = await self.config.guild(member.guild).toggle_unban()
         if enabled and toggle_unban:
             # logger.info("{} was unbanned from the server.".format(user.display_name))
-            channel = discord.utils.find(lambda c: c.name == (await self.config.guild(guild).channel()),
-                                         guild.channels)
+            channel_name = await self.config.guild(member.guild).channel()
+            channel = discord.utils.find(lambda c: c.name == channel_name, guild.channels)
             # channel = discord.utils.get(guild.channels, name=str(await self.config.guild(guild).channel()),
             #                             type=ChannelType.text)
             embed = discord.Embed(title="Member Unbanned", color=0x8080ff)
@@ -116,8 +116,8 @@ class Sentinel(commands.Cog):
         ignored_roles = await self.config.guild(before.guild).ignored_roles()
         if enabled and toggle_name_change and before.display_name != after.display_name or before.name != after.name:
             # logger.info("{} changed their name to {}.".format(before.display_name, after.display_name))
-            channel = discord.utils.find(lambda c: c.name == (await self.config.guild(after.guild).channel()),
-                                         after.guild.channels)
+            channel_name = await self.config.guild(after.guild).channel()
+            channel = discord.utils.find(lambda c: c.name == channel_name, after.guild.channels)
             # channel = discord.utils.get(after.guild.channels, name=str(await self.config.guild(after.guild).channel()),
             #                             type=ChannelType.text)
             embed = discord.Embed(title="User changed their name",
@@ -152,8 +152,8 @@ class Sentinel(commands.Cog):
                 return
             else:
                 # logger.info("{} roles changed from {} to {}.".format(after.display_name, old_roles, new_roles))
-                channel = discord.utils.find(lambda c: c.name == (await self.config.guild(after.guild).channel()),
-                                             after.guild.channels)
+                channel_name = await self.config.guild(after.guild).channel()
+                channel = discord.utils.find(lambda c: c.name == channel_name, after.guild.channels)
                 # channel = discord.utils.get(after.guild.channels,
                 #                             name=str(await self.config.guild(after.guild).channel()),
                 #                             type=ChannelType.text)
@@ -178,8 +178,8 @@ class Sentinel(commands.Cog):
             if not after.author.bot:
                 # logger.info("{} changed the message {} to {}.".format(after.author.display_name, before.content,
                 #                                                       after.content))
-                channel = discord.utils.find(lambda c: c.name == (await self.config.guild(after.guild).channel()),
-                                             after.guild.channels)
+                channel_name = await self.config.guild(after.guild).channel()
+                channel = discord.utils.find(lambda c: c.name == channel_name, after.guild.channels)
                 # channel = discord.utils.get(after.guild.channels,
                 #                             name=str(await self.config.guild(after.guild).channel()),
                 #                             type=ChannelType.text)
