f22328e5e24ee5cb4d812a811823f6ad1c3e36b9
Whiskey <ext.whiskey@gmail.com>
2021-07-13 20:35:11 -0400

    -so much of it

--- gameserverquery/gameserverquery.py
+++ gameserverquery/gameserverquery.py
@@ -72,14 +72,16 @@ class GameServerQuery(commands.Cog):
     async def server_poll(self):
         info = await self.query_server()
         if info.player_count == 0:
-            activity = discord.BaseActivity(
-                party={'id': 'Fight Club', 'size': [int(info.player_count), int(info.max_players)]},
-                state="Fight Club", details="Join the salt!")
+            activity = discord.Activity(name="Playing life is feudal", emoji="sinner",
+                                        party={'id': 'Fight Club',
+                                               'size': [int(info.player_count), int(info.max_players)]},
+                                        state="Fight Club", details="Join the salt!")
             await self.bot.change_presence(status=discord.Status.idle, activity=activity)
         else:
-            activity = discord.BaseActivity(
-                party={'id': 'Fight Club', 'size': [int(info.player_count), int(info.max_players)]},
-                state="Fight Club", details="Join the salt!")
+            activity = discord.Activity(name="Playing life is feudal", emoji="sinner",
+                                        party={'id': 'Fight Club',
+                                               'size': [int(info.player_count), int(info.max_players)]},
+                                        state="Fight Club", details="Join the salt!")
             await self.bot.change_presence(status=discord.Status.online, activity=activity)
 
     @commands.guild_only()
